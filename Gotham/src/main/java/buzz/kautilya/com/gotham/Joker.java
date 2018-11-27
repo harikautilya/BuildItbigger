81893302bf05fea06c8045159d81e70a4d8e812b
kautilya <harikautilya@gmail.com>
2018-11-27 20:14:10 +0530

    Step 0

---
+++ Gotham/src/main/java/buzz/kautilya/com/gotham/Joker.java
@@ -0,0 +1,7 @@
+package buzz.kautilya.com.gotham;
+
+public class Joker {
+    String laugh(){
+        return "Why so serious";
+    }
+}
---
+++ bridge/src/main/java/buzz/kautilya/com/bridge/AttackActivity.java
@@ -0,0 +1,13 @@
+package buzz.kautilya.com.bridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attack);
+    }
+}
