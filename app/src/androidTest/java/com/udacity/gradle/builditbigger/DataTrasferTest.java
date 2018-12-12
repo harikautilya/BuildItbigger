e7aa073e4e87613634bdfe635bea689bd3f407c3
kautilya <harikautilya@gmail.com>
2018-12-12 21:13:05 +0530

    Changing test cases

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/DataTrasferTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/DataTrasferTest.java
@@ -1,22 +1,39 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.InstrumentationTestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(AndroidJUnit4.class)
-public class DataTrasferTest {
+public class DataTrasferTest extends InstrumentationTestCase {
+
+    ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class, true);
 
     @Test
-    public void testDataRequest(){
-        new EndpointsAsyncTask(new EndpointsAsyncTask.EndpointsAsyncTaskInteractor() {
+    public void testDataRequest() throws Throwable {
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        runTestOnUiThread(new Runnable() {
+
             @Override
-            public void result(String name) {
-                assertEquals(name, "Why so serious");
+            public void run() {
+                new EndpointsAsyncTask(new EndpointsAsyncTask.EndpointsAsyncTaskInteractor() {
+                    @Override
+                    public void result(String name) {
+                        assertEquals(name, "Why so serious");
+                        signal.countDown();
+                    }
+                }).execute();
             }
-        }).execute();
+        });
+        signal.await(30, TimeUnit.SECONDS);
     }
 }
